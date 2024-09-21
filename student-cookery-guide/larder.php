cfa079f2f6f0f784936d34f52c6dd27564eff360
rowlandcoping <rowlandcoping@gmail.com>
2024-09-21 05:01:24 +0100

    ADD: formatting for larder and recipe pages, plus captions.

--- student-cookery-guide/larder.php
+++ student-cookery-guide/larder.php
@@ -15,77 +15,94 @@
 		<div class="page">
 			<?php require_once($banner); ?>
 			<div class="main">
-				<div class="container">	
+                <div id="liveblog-container">
+                    <div id="content-container">
 					<!--this one is just a series of boxes maybe add expandable comments for each review-->	
-					<div class="homecontent">
-						<!--intro-->
-						<div class ="fltright">
-							<img src="/assets/images/bread.jpg" width="350">
-							<p>Some bread, yesterday.</p>
-						</div>
-						<div class="review">
-							<h1>Cupboard Love</h1>
-							<p>
-                            If you are to become a Cookery Expert like Rowland, who can knock delicious stuff up in a jiffy, then you need a basic larder.
-                            </p>
-							<p>
-						    I would use a Ready Steady Cook reference here (Percy Pepper and so on), but none of you will have the slightest idea what I'm on about. 
-							<p>
-                            Hey ho. Crack on then eh?
-                            </p>
-						</div>
-                        
-					</div>
-                    <div class="homecontent">
-                        <div class="review">
-                        <h2>Larder Basics</h2>
-                            <h3>Onions</h3>
-                            Onions form a base for pretty much anything.  If you have onions, you're making a start on most recipes.  If you don't like onions I feel bad for you.
-                            <h3>Garlic</h3>
-                            Most ready meals and processed foods list garlic as an ingredient. This is because it provides instant flavour.
-                            <br>NB: garlic can make your breath smell bad.  Although it is an outstanding ingredient, use it with caution, especially if you're just about to go out on the pull.
-                            <h3>Pasta</h3>
-                            You can cook pasta in 10 minutes.  Then you can add things to it.  Hey presto, you've done cooking. Always have pasta.
-                            <h3>Tinned Tomatoes</h3>
-                            In combination with pasta, onion, and one or more of the aforementioned herbs, if you have all these things on hand you basically have instant food.
-                            <h3>Olive Oil</h3>
-                            If you only have one oil, get some basic olive oil (not extra virgin).  You can use it for frying (you don't need much), roasting, baking, making flatbread, or you can even use it as a dressing at a push.
-                            <br>The Extra Virgin variety is really just for dressings.  I always put a bit in my pasta to add that lovely grassy flavour.
-                            <h3>Bread</h3>
-                            Bread begats toast.  It incorporates sandwiches and cheese on toast and toasties and many other toast base things.
-                            <h3>I Can't Believe It's Not I Can't Believe It's Not Butter</h3>
-                            Buy whatever cheap non-butter containing spread your supermarket has to offer.  Use in combination with toast or bread or crumpets or anything really.
-                            <h3>Plain Flour</h3>
-                            You can use this to make flatbread, basic sauces and thicken things like stews.  It might not seem like you need it now.  But you probably will.
-                            
-                            
-                            <h2>Herbs and Spices</h2>
-                            These are the little fellows that provide you with everything a Toby Carvery lacks - that's right, flavour!  
-                            <br>There are many, many of them, but here are some of the basics that you can use in almost any situation to make life better.  
-                            <br>As you try more ambitious recipes, you will learn more about what does and doesn't work together.  Hey presto, you can cook!
-                            <br>Purchase dried herbs for your larder.  If you want to get fancy, buy them fresh for particular recipes.  If you think you're a gardener and want to brighten the place up a bit, you can even grow these in a pot on your window.
-                            <br>NB: Growing herbs takes time, often weeks or months.  Plants often die in student houses. I would advise not to rely too hard on this method.
-                            <h3>Black Pepper</h3>
-                            Invest in a black pepper grinder and put more black pepper corns in it when it runs out.  Use this in everything.
-                            <br>As a first year, I had catered halls, but I brought one of these with me every night to dinner just so the food tasted of... something.
-                            <br>Initially my cohort was sceptical, but soon they were all borrowing it!
-                            <h3>Salt</h3>
-                            Ideally use sea salt rather than pure msg. Either way, use this ingredient very sparingly.
-                            <h3>Oregano</h3>
-                            A strong, fragrent herb, it adds an extra layer of flavour to any tomato sauce and is also great with pretty much any roasted meat.
-                            <h3>Basil</h3>
-                            Adds a sweet, peppery, aniseedy flavour and is particularly excellent with any tomato based sauce.  Fresh basil also works great in salads.
-                            <br>NB: Oregano and Basil are often found in combination in 'Mixed Herbs'.  You can chuck mixed herbs in about anything.
-                            <h3>Cumin</h3>
-                            Cumin is a spice which plays a important role in flavouring curries (could be said to be the taste of curry!).  You can add it to all sorts to add that warm, earthy flavour.
-                            <br> It is also a key component in a lot of mexican cuisine, for example the fahita kits that cost about billion pounds for a small sachet and 4 tortillas are mostly cumin.
-                            <h3>Paprika</h3>
-                            Paprika is basically ground up red peppers, and is a splendid addition to most soups or stews.  I used to add it to chicken and mushroom pasta 'n' sauce to great effect.
-                            <br>Even better is its sporty cousin smoked paprika, which adds a smokey chipolte flavour to your cooking - but use smoked paprika sparingly, less is more with this one.
-                            <h3>Cayenne Pepper</h3>
-                            Cayenne pepper adds heat to dishes along with a lovely smooth chilli flavour.  If you want to add a bit of spice it's the most delicious way of doing so.
-                            <br>If you want more kick you can always pick up some hot chilli powder.
+                        <div class="blogcont">
+                            <!--intro-->
+                            <div class ="blogimgright">
+                                <img src="/assets/images/bread.jpg" width="350">
+                                <p>Some bread, yesterday.</p>
+                            </div>
+                            <div class="blogentry">
+                                <h1>Cupboard Love</h1>
+                                <p>
+                                If you are to become a Cookery Expert like Rowland, who can knock delicious stuff up in a jiffy, then you need a basic larder.
+                                </p>
+                                <p>
+                                I would use a Ready Steady Cook reference here (Percy Pepper and so on), but none of you will have the slightest idea what I'm on about. 
+                                <p>
+                                Hey ho. Crack on then eh?
+                                </p>
+                            </div>                            
                         </div>
+                        <div class="linethin"></div>
+                        <div class="nav-container">
+                            <div class="left-nav-button"><a href="/student-cookery-guide/helpful-hacks.php"><h3>&#60;&#60; HACKS</h3></a></div>
+                            <div class="centre-nav-button"><a href="/student-cookery-guide.php"><h3>HOME</h3></a></div>
+                            <div class="right-nav-button"><a href="/student-cookery-guide/student-recipes.php"><h3>RECIPES &#62;&#62;</h3></a></div>
+                        </div>
+                        <div class="linethin"></div>
+                        <div class="blogcont">
+                            <div class="blogentry">
+                                <div class="segment">
+                                    <h2>Larder Basics</h2>
+                                    <h3>Onions</h3>
+                                    <p>Onions form a base for pretty much anything.  If you have onions, you're making a start on most recipes.  If you don't like onions I feel bad for you.</p>
+                                    <h3>Garlic</h3>
+                                    <p>Most ready meals and processed foods list garlic as an ingredient. This is because it provides instant flavour.</p>
+                                    <p><em>NB: garlic can make your breath smell bad.  Although it is an outstanding ingredient, use it with caution, especially if you're just about to go out.</em></p>
+                                    <h3>Pasta</h3>
+                                    <p>You can cook pasta in 10 minutes.  Then you can add things to it.  Hey presto, you've done cooking. Always have pasta.</p>
+                                    <h3>Tinned Tomatoes</h3>
+                                    <p>In combination with pasta, onion, and one or more of the aforementioned herbs, if you have all these things on hand you basically have instant food.</p>
+                                    <h3>Olive Oil</h3>
+                                    <p>If you only have one oil, get some basic olive oil (not extra virgin).</p>
+                                    <p>You can use it for frying (you don't need much), roasting, baking, making flatbread, or you can even use it as a dressing at a push.</p>
+                                    <p>The Extra Virgin variety is really just for dressings.  I always put a bit in my pasta to add that lovely grassy flavour.</p>
+                                    <h3>Bread</h3>
+                                    <p>Bread begets toast.  It incorporates sandwiches and cheese on toast and toasties and many other toast base things.</p>
+                                    <h3>I Can't Believe It's Not I Can't Believe It's Not Butter</h3>
+                                    <p>Buy whatever cheap non-butter containing spread your supermarket has to offer.  Use in combination with toast or bread or crumpets or anything really.</p>
+                                    <h3>Plain Flour</h3>
+                                    <p>You can use this to make flatbread, basic sauces and thicken things like stews.  It might not seem like you need it now.  But you probably will.</p>
+                                </div>
+                                <div class="segment">
+                                    <h2>Herbs and Spices</h2>
+                                    <p>These are the little fellows that provide you with everything a Toby Carvery lacks - that's right, flavour!</p>
+                                    <p>There are many, many of them, but here are some of the basics that you can use in almost any situation to make life better. </p>
+                                    <p>As you try more ambitious recipes, you will learn more about what does and doesn't work together.  Hey presto, you can cook!</p>
+                                    <p>Purchase dried herbs for your larder.  If you want to get fancy, buy them fresh for particular recipes.  If you think you're a gardener and want to brighten the place up a bit, you can even grow these in a pot on your window.</p>
+                                    <p><em>NB: Growing herbs takes time, often weeks or months.  Plants often die in student houses. I would advise not to rely too hard on this method.</em></p>
+                                    <h3>Black Pepper</h3>
+                                    <p>Invest in a black pepper grinder and buy more black pepper corns to put in it when it runs out.  Use this in everything.</p>
+                                    <p><em>NB: If you live in catered halls bring this with you to dinner, so you can make your food actually tastes of something.</em></p>
+                                    <h3>Salt</h3>
+                                    <p>Ideally use sea salt rather than pure msg. Either way, use this ingredient very sparingly.</p>
+                                    <h3>Oregano</h3>
+                                    <p>A strong, fragrent herb, it adds an extra layer of flavour to any tomato sauce and is also great with pretty much any roasted meat.</p>
+                                    <h3>Basil</h3>
+                                    <p>Adds a sweet, peppery, aniseedy flavour and is particularly excellent with any tomato based sauce.  Fresh basil also works great in salads.</p>
+                                    <p><em>NB: Oregano and Basil are often found in combination in 'Mixed Herbs'.  You can chuck mixed herbs in about anything.</em></p>
+                                    <h3>Cumin</h3>
+                                    <p>Cumin is a spice which plays a important role in flavouring curries (could be said to be the taste of curry!).  You can add it to all sorts to add that warm, earthy flavour.</p>
+                                    <p> It is also a key component in a lot of mexican cuisine, for example the fahita kits that cost about billion pounds for a small sachet and 4 tortillas are mostly cumin.</p>
+                                    <h3>Paprika</h3>
+                                    <p>Paprika is basically ground up red peppers, and is a splendid addition to most soups or stews.  I used to add it to chicken and mushroom pasta 'n' sauce to great effect.</p>
+                                    <p>Even better is its sporty cousin smoked paprika, which adds a smokey chipolte flavour to your cooking - but use smoked paprika sparingly, less is more with this one.</p>
+                                    <h3>Cayenne Pepper</h3>
+                                    <p>Cayenne pepper adds heat to dishes along with a lovely smooth chilli flavour.  If you want to add a bit of spice it's the most delicious way of doing so.</p>
+                                    <p>If you want more kick you can always pick up some hot chilli powder.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="linethin"></div>
+                        <div class="nav-container">
+                            <div class="left-nav-button"><a href="/student-cookery-guide/helpful-hacks.php"><h3>&#60;&#60; HACKS</h3></a></div>
+                            <div class="centre-nav-button"><a href="/student-cookery-guide.php"><h3>HOME</h3></a></div>
+                            <div class="right-nav-button"><a href="/student-cookery-guide/student-recipes.php"><h3>RECIPES &#62;&#62;</h3></a></div>
+                        </div>
+                        <div class="linethin"></div>
                     </div>
 				</div>
 			</div>
--- student-cookery-guide/student-recipes.php
+++ student-cookery-guide/student-recipes.php
@@ -15,66 +15,85 @@
 		<div class="page">
 			<?php require_once($banner); ?>
 			<div class="main">
-				<div class="container">	
+            <div id="liveblog-container">
+                    <div id="content-container">
 					<!--this one is just a series of boxes maybe add expandable comments for each review-->	
-					<div class="homecontent">
-						<!--intro-->
-						<div class ="fltright">
-							<img src="/assets/images/equipment.webp" width="350">
-							<p>The real thing was so much worse than this stock image.</p>
-						</div>
-						<div class="review">
-							<h1>Recipe Ideas</h1>
-							<p>
-                            Yeah, this is what you were waiting for.  Actual new content, that isn't just a thrown together mess, or a barely coherent wall of text.
-                            </p>
-							<p>
-						    I've split these into categories, some of which are little more than guides, some already exist right here, and some have needed doing a while. 
-							<p>
-                            Some are highly self-indulgent.  Additionally I just realised there is not much here for vegetarians.  Honestly it's not really written for a vegetarian.
-                            </p>
-                            <p>
-                            NB: Lily you'd tell me if you were vegetarian again, right?
-                            </p>
-						</div>
-                        
-					</div>
-                    <div class="homecontent">
-                        <div class="review">
-                        <h2>Pasta Dishes</h2>
-                            With pasta dishes if you are having leftovers I recommend re-heating the sauce bit and cooking the pasta you need as you go.
-                            <br>Pasta is really not great re-heated.
-                            <h3>Italian Sausage Pasta</h3>
-                            So simple, and originally made using sausages from a lovely italian deli down my road.  Nowadays I just chuck anything in there.
-                            <h3>Chicken and Bacon pasta</h3>
-                            The chicken and bacon combo is great and it has a lovely smoky flavour.
-                            <h3>Chicken with Lemon and Tarragon</h3>
-                            Included largely because it is already on this website.
-                            <h3>Pasta & Pesto</h3>
-                            Pesto from a jar. Includes some handy variations to make your life better.
-                            <h3>Spaghetti Bolognese</h3>
-                            If you only take one thing away from this mess of a feature, make it this recipe.  I'll do lasagne too one day, promise.
-                            <h2>Bread Based stuff</h2>
-                            <h3>The Bacon Sandwich</h3>
-                            My Favorite version of this, plus variations
-                            <h3>Cheese on Toast</h3>
-                            Did you know you can cook this in an air fryer?  I didn't, until my grill broke
-                            <h3>Ham Cheese and Pickle</h3>
-                            This is a rapid go-to sandwich that will change your life.
-                            <h3>Beans on Toast</h3>
-                            I'm including this on account of some useful methodology.
-                                                      
-                            
-                            <h2>Other Dishes</h2>
-                            <h3>Sausage Pie</h3>
-                            A Saturday night special, handed down over generations, from back in the day when there were only four TV channels.  And when people actually watched TV for that matter.
-                            <h3>Chilli</h3>
-                            It's a very simple recipe really, but you could make it simpler by using a supermarket packet chilli mix rather than all the individual ones.  It won't be as good though.
-                            <h3>Student Fahita Wraps</h3>
-                            Using only the stuff in your <a href="/student-cookery-guide/larder.php">larder</a>, no more expensive fahita kits.
+                        <div class="blogcont">
+                            <!--intro-->
+                            <div class ="blogimgright">
+                                <img src="/assets/images/recipe.jpg" width="350">
+                                <p>Actually I have no idea what I put in this.</p>
+                            </div>
+                            <div class="blogentry">
+                                <h1>Recipe Ideas</h1>
+                                <p>
+                                Yeah, this is what you were waiting for.  Actual new content that isn't just a thrown together mess, or a barely coherent wall of text.
+                                </p>
+                                <p>
+                                I've split these into categories, some of which are little more than guides, some already exist right here, and some have needed doing a while. 
+                                <p>
+                                Some are highly self-indulgent.  Additionally I just realised there is not much here for vegetarians.  Honestly it's not really written for a vegetarian.
+                                </p>
+                                <p><em>
+                                NB: Lily, you'd tell me if you were vegetarian again, right?
+                                </em></p>
+                            </div>                            
                         </div>
-                    </div>
-				</div>
+                        <div class="linethin"></div>
+                        <div class="nav-container">
+                            <div class="left-nav-button"><a href="/student-cookery-guide/larder.php"><h3>&#60;&#60; LARDER</h3></a></div>
+                            <div class="right-nav-button"><a href="/student-cookery-guide.php"><h3>HOME</h3></a></div>
+                        </div>
+                        <div class="linethin"></div>
+                        <div class="blogcont">
+                            <div class="blogentry">
+                                <div class="segment">
+                                <h2>Pasta Dishes</h2>
+                                    <p>With pasta dishes if you are having leftovers I recommend re-heating the sauce bit and cooking the pasta you need as you go.</p>
+                                    <p>Pasta is really not great re-heated.</p>
+                                    <h3>Italian Sausage Pasta</h3>
+                                    <p>So simple, and originally made using sausages from a lovely italian deli down my road.  Nowadays I just chuck anything in there.</p>
+                                    <h3>Chicken and Bacon pasta</h3>
+                                    <p>The chicken and bacon combo is great and it has a lovely smoky flavour.</p>
+                                    <h3><a href="/recipe/chicken-with-lemon-and-tarragon" target="_blank">Chicken with Lemon and Tarragon</a></h3>
+                                    <p>Included largely because it is already on this website.</p>
+                                    <h3>Pasta & Pesto</h3>
+                                    <p>Pesto from a jar. Includes some handy variations to make your life better.</p>
+                                    <h3>Spaghetti Bolognese</h3>
+                                    <p>If you only take one thing away from this mess of a feature, make it this recipe.  I'll do lasagne too one day, promise.</p>
+                                </div>
+                                <div class="segment">
+                                    <h2>Bread Based stuff</h2>
+                                    <h3>The Bacon Sandwich</h3>
+                                    <p>My Favorite version of this, plus variations.</p>
+                                    <h3>Cheese on Toast</h3>
+                                    <p>Did you know you can cook this in an air fryer?  I didn't, until my grill broke.</p>
+                                    <h3>Ham Cheese and Pickle</h3>
+                                    <p>This is a rapid go-to sandwich that will change your life.</p>
+                                    <h3>Beans on Toast</h3>
+                                    <p>I'm including this on account of some useful methodology.</p>
+                                </div>
+                                <div class="segment">
+                                    <h2>Other Dishes</h2>
+                                    <h3>Sausage Pie</h3>
+                                    <p>A Saturday night special, handed down over generations, from back in the day when there were only four TV channels.  And when people actually watched TV for that matter.</p>
+                                    <h3><a href="/recipe/chilli-con-carne" target="_blank">Chilli</a></h3>
+                                    <p>It's a very simple recipe really, but you could make it simpler by using a supermarket packet chilli mix rather than all the individual ones.  It won't be near as good though.</p>
+                                    <h3>Student Fahita Wraps</h3>
+                                    <p>Using only the stuff in your <a href="/student-cookery-guide/larder.php" target="_blank">larder</a>! Say goodbye to expensive and rubbish fahita kits.</p>
+                                    <h3>Tray Bake Thing</h3>
+                                    <p>This is a regular go-to.  Take a cheap cut of chicken (or whatever), a load of veg, put it in the oven for a while, then eat it.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="linethin"></div>
+                        <div class="nav-container">
+                            <div class="left-nav-button"><a href="/student-cookery-guide/larder.php"><h3>&#60;&#60; LARDER</h3></a></div>
+                            <div class="right-nav-button"><a href="/student-cookery-guide.php"><h3>HOME</h3></a></div>
+                        </div>
+                        <div class="linethin"></div>
+                    </div>                    
+                </div>
 			</div>
 		</div>
 	</body>
